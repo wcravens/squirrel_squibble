508af314618130fe1d692568b7c14a445c9dacd9
Wes Cravens <wcravens@cortex-it.com>
2022-07-08 02:28:25 -0500

    Update root rendering technique to comply with most recent version of React

--- browser_client/src/index.js
+++ browser_client/src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import App from './App';
-//import 'bootstrap/dist/css/bootstrap.css'
 import './custom.scss';
 
-console.log( "Build: " + process.env.REACT_APP_BRANCH + ":"  + process.env.REACT_APP_VERSION )
 const AppInfo = {
   name: process.env.REACT_APP_NAME,
   version: process.env.REACT_APP_VERSION,
   branch: process.env.REACT_APP_BRANCH,
   build: process.env.REACT_APP_BUILD
 }
+console.log( "AppInfo:" + JSON.stringify( AppInfo ) )
 
-ReactDOM.render(
+const container = document.getElementById( 'root' )
+const root = createRoot( container )
+root.render(
   <React.StrictMode>
     <App />
     <hr/>
     <pre>{AppInfo.name} {AppInfo.version}, {AppInfo.branch} Build: {AppInfo.build}</pre>
   </React.StrictMode>,
-  document.getElementById('root')
 );
