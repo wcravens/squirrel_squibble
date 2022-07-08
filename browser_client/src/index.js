2f4b63460a8a1e99c1dbf4441776897437570eed
Wes Cravens <wcravens@cortex-it.com>
2022-07-08 01:51:34 -0500

    Add branch and commit version to the web app

--- browser_client/src/index.js
+++ browser_client/src/index.js
@@ -4,9 +4,13 @@ import App from './App';
 //import 'bootstrap/dist/css/bootstrap.css'
 import './custom.scss';
 
+console.log( "Build: " + process.env.REACT_APP_BRANCH + ":"  + process.env.REACT_APP_VERSION )
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <a href="%PUBLIC_URL%/bootstrap-5.2.0-beta1-examples">Bootstrap Examples</a>
+    <hr/>
+    <pre>Squirrel Squibble Build: {process.env.REACT_APP_BRANCH} {process.env.REACT_APP_VERSION}</pre>
   </React.StrictMode>,
   document.getElementById('root')
 );
