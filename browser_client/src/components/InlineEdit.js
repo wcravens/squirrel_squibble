fcf7d553a70c829771661f1b7b042f437092e202
Abhi Vinnakota <abhi.vinnakota5@gmail.com>
2022-07-13 14:34:35 -0500

    Refactored code so that inline and textarea is one component

--- browser_client/src/components/InlineEdit.js
+++ browser_client/src/components/InlineEdit.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import './InlineEdit.css'
+import './TextAreaEdit.css'
 
-const InlineEdit = ({ defaultValue }) => {
+
+
+const InlineEdit = ({defaultValue, type}) => {
   const [ value, setValue ] = useState( '' );
   const [ pure, setPure ] = useState( true )
   const onFocus   = event => { if (pure) { setValue(''); setPure( false ) } };
-  const onChange  = event => setValue( event.target.value )
-  const onKeyDown = event => { if ( event.key === "Enter" || event.key === "Escape" ) { event.target.blur(); } }
+  const onChange  = event => setValue( event.target.value );
+  const onKeyDown = event => { if ( event.key === "Enter" || event.key === "Escape" ) { event.target.blur(); } };
 
-  return (
-    <input
-      type = "text"
-      aria-label = "Field name"
-      value = { pure ? defaultValue : value }
-      className = { pure ? 'pure' : '' }
-      onFocus   = { onFocus }
-      onChange  = { onChange }
-      onKeyDown = { onKeyDown }
-    />
-  )
+  const Label = ({tag, ...other}) => {
+    const component = {tag: tag};
+
+    return (<component.tag {...other} aria-label = "Field name" value = { pure ? defaultValue : value } className = { pure ? 'pure' : '' } onFocus   = { onFocus } onChange  = { onChange } onKeyDown = { onKeyDown }></component.tag>);
+  };
+
+  if (type === "input") {
+    return (
+        <Label tag = {type} type = "text"/>
+    );
+  } else if (type === "textarea") {
+    return (
+        <Label tag = {type} rows={3}/>
+    );
+  }
+  
 }
 
 export default InlineEdit
