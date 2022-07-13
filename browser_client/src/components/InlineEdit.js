d3d2ff90d2a3f151e4767f1dfa2c3ead2f5fe649
Wes Cravens <wcravens@cortex-it.com>
2022-07-13 10:47:43 -0500

    Make InlineEdit color grey(sic) until focused.

--- browser_client/src/components/InlineEdit.js
+++ browser_client/src/components/InlineEdit.js
@@ -4,7 +4,7 @@ import './InlineEdit.css'
 const InlineEdit = ({ defaultValue }) => {
   const [ value, setValue ] = useState( '' );
   const [ pure, setPure ] = useState( true )
-  //const onFocus   = event => { if (pure) { setValue(''); setPure( false ) } };
+  const onFocus   = event => { if (pure) { setValue(''); setPure( false ) } };
   const onChange  = event => setValue( event.target.value )
   const onKeyDown = event => { if ( event.key === "Enter" || event.key === "Escape" ) { event.target.blur(); } }
 
@@ -13,8 +13,8 @@ const InlineEdit = ({ defaultValue }) => {
       type = "text"
       aria-label = "Field name"
       value = { pure ? defaultValue : value }
-      className = { 'pure' }
-      //onFocus   = { onFocus }
+      className = { pure ? 'pure' : '' }
+      onFocus   = { onFocus }
       onChange  = { onChange }
       onKeyDown = { onKeyDown }
     />
