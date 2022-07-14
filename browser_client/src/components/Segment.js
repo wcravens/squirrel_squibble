6ba1b6e6e99b69f8c9c464806cc4a35c19db4f5c
Wes Cravens <wcravens@cortex-it.com>
2022-07-14 00:24:46 -0500

    Extract styles into seperate definition.

--- browser_client/src/components/Segment.js
+++ browser_client/src/components/Segment.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react'
 
 const Segment = ( props ) => {
+
   const nestedSegments = ( props.scion || [] ).map( seg => {
     return  <Segment { ...seg } />
   });
+
+  const styles = {
+    border: '1px solid blue',
+    marginLeft: '10px'
+  }
+
   return (
-    <article style={ { border: '1px solid blue' } }>
+    <article style={styles}  >
       <pre>{ props.type }</pre>
       <header>{ props.header }</header>
       <p>{ props.content }</p>
