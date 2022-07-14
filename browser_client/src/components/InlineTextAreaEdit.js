a258cfdd1af8f5e2ced32190a2b3cf61ad91b2a3
Wes Cravens <wcravens@cortex-it.com>
2022-07-13 20:19:42 -0500

    Inline Edit Components Highlight ring and Dynamic scaling textarea.

--- browser_client/src/components/InlineTextAreaEdit.js
+++ browser_client/src/components/InlineTextAreaEdit.js
@@ -4,9 +4,20 @@ import './InlineTextAreaEdit.css'
 
 const InlineTextAreaEdit = ( { defaultValue } ) => {
   const _ = _InlineEdit( defaultValue );
+  const onInput = (e) => {
+    if (e.target.scrollHeight > 33) {
+      e.target.style.height = "5px";
+      e.target.style.height = (e.target.scrollHeight - 16) + "px";
+    }
+  }
   return (
-    <textarea {..._.props} />
-  )
+    <textarea
+      rows = {1}
+      cols = {82}
+      onInput = { onInput }
+      {..._.props}
+    />
+  );
 }
 
 export default InlineTextAreaEdit;
