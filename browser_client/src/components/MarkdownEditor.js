6d2b7b9daf83854f7cda9b5076ea0e6d81be0118
Wes Cravens <wcravens@cortex-it.com>
2022-07-13 21:33:05 -0500

    Adding more Markdown Editors

--- browser_client/src/components/MarkdownEditor.js
+++ browser_client/src/components/MarkdownEditor.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import MDEditor from '@uiw/react-md-editor'
+import rehypeSanitize from "rehype-sanitize";
 
 export default function MarkdownEditor() {
-  const [value, setValue] = React.useState("**Hello world!!!**");
+  const [value, setValue] = React.useState('');
   return (
     <div className="container">
       <MDEditor
         value={value}
         onChange={setValue}
+        previewOptions={{
+          rehypePlugins: [[rehypeSanitize]],
+        }}
       />
       <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
     </div>
