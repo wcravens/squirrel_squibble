4551fdc68880cbbff93febf4ff96311b942212c7
Wes Cravens <wcravens@cortex-it.com>
2022-07-30 04:55:44 -0500

    Theme switcher WIP

--- Sandbox/app-webpack-from-scratch/src/index.js
+++ Sandbox/app-webpack-from-scratch/src/index.js
@@ -1,2 +1,21 @@
-import PACKAGE from '../package.json'
-document.querySelector('#container').innerHTML = `<pre>${PACKAGE.name} ${VERSION}\nLast Commit: ${COMMITHASH} ${LASTCOMMITDATETIME}</pre>`
+import PACKAGE from '../package.json' assert { type: 'json' };
+import * as bootstrapjs from 'bootstrap'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+
+export const THEME_SHEETS = {
+  bootstrap   : import( 'bootstrap/dist/css/bootstrap.min.css' ),
+  zephyr      : import( 'bootswatch/dist/zephyr/bootstrap.min.css' ),
+  flatly      : import( 'bootswatch/dist/flatly/bootstrap.min.css' ),
+  cosmo       : import( 'bootswatch/dist/cosmo/bootstrap.min.css' ),
+  sketchy     : import( 'bootswatch/dist/sketchy/bootstrap.min.css' )
+}
+
+
+const populateFooterWithAppNameAndVersion = () => document.querySelector('#mainFooter').innerHTML =
+  `<pre>${PACKAGE.name} ${VERSION}\nLast Commit: ${COMMITHASH} ${LASTCOMMITDATETIME}</pre>`
+
+const updateThemeStyleElement = css  => document.head.insertAdjacentHTML( 'beforeend', `<style>${css}</style>` )
+
+export const changeTheme = theme => THEME_SHEETS[theme].then( updateThemeStyleElement )
+
+//changeTheme( 'zephyr' )
--- Sandbox/app-webpack-from-scratch/webpack.config.js
+++ Sandbox/app-webpack-from-scratch/webpack.config.js
@@ -1,13 +1,37 @@
-const webpack = require('webpack');
-const PACKAGE = require('./package.json')
-const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import webpack                from 'webpack'
+import PACKAGE                from './package.json' assert { type: 'json' }
+import { GitRevisionPlugin }  from 'git-revision-webpack-plugin'
+import HtmlWebpackPlugin      from 'html-webpack-plugin'
+
+
 const gitRevisionPlugin = new GitRevisionPlugin( {
   branch: true,
 });
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          { loader: "postcss-loader", options: { postcssOptions: { plugins: ()=> [ require('autoprefixer') ] } } },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
   mode: 'development',
+  entry: './src/index.js',
+  output: {
+    libraryTarget: 'var',
+    library:  'Squirrel_Squabble'
+  },
   devServer: {
     port: 8001,
     hot: true
@@ -15,6 +39,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: JSON.stringify( PACKAGE.name + ' ' + gitRevisionPlugin.version() ),
+      startupTheme: JSON.stringify('zephyr'),
       template: './public/index.html'
     }),
     gitRevisionPlugin,
