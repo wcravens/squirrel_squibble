9cb2ebd6b8a5ef408531b26dabe1088a057fea24
Wes Cravens <wcravens@cortex-it.com>
2022-07-31 06:21:14 -0500

    Tinkering with JSON validation.

--- Sandbox/Squibble/Application/Core/Validator.js
+++ Sandbox/Squibble/Application/Core/Validator.js
@@ -2,13 +2,15 @@ import Ajv from 'ajv/dist/2020.js'
 import addFormats from "ajv-formats"
 import  $RefParser from "json-schema-ref-parser";
 
+import Entity from './Schema/Entity.json' assert { type: 'json' }
+import Action from './Schema/Action.json' assert { type: 'json' }
+import Application from './Schema/Application.json' assert { type: 'json' }
+import User from './Schema/User.json' assert { type: 'json' }
+import Ticket from './Schema/Ticket.json' assert { type: 'json' }
+import Project from './Schema/Project.json' assert { type: 'json' }
 
-
-const SchemaList = [ ENTITY_DECORATOR_SCHEMA, JIRA_TICKET_SCHEMA, PROJECT_SCHEMA, USER_SCHEMA, ACTION_SCHEMA, APPLICATION_SCHEMA ]
-
+const Schemalist = [ Entity, Action, Application, User, Ticket, Project  ]
 
 const ajv = new Ajv()
 addFormats( ajv )
-ajv.addSchema( SchemaList )
-
-
+ajv.addSchema( Schemalist )
