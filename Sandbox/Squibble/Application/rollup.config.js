fbd0242d369fb9a160d87ebf45c08584ebbc8d28
Wes Cravens <wbc3@illinois.edu>
2022-08-03 10:53:47 -0500

    Tidy up.

--- Sandbox/Squibble/Application/rollup.config.js
+++ Sandbox/Squibble/Application/rollup.config.js
@@ -1,12 +1,6 @@
 import gitInfo from 'rollup-plugin-git-info'
 import eslint from '@rollup/plugin-eslint'
 
-//import replace from '@rollup/plugin-replace'
-//import { babel } from '@rollup/plugin-babel'
-//import json   from '@rollup/plugin-json'
-//import { nodeResolve } from '@rollup/plugin-node-resolve'
-//import commonjs from '@rollup/plugin-commonjs'
-
 export default {
   input: 'app.js',
   output: {
