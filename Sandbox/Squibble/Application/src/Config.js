50e36da64617ab51533384ce1a6d9f7d2e64a2dc
Wes Cravens <wbc3@illinois.edu>
2022-08-03 10:34:54 -0500

    Manage Config git info with rollup plugin.

--- Sandbox/Squibble/Application/src/Config.js
+++ Sandbox/Squibble/Application/src/Config.js
@@ -1,22 +1,4 @@
-import PACKAGE  from '../package.json' assert { type: 'json' }
-import SimpleGit from 'simple-git'
+import PACKAGE  from '../package.json'
+// Note: package.json is supplemented via rollup git-info plugin.
 
-export const initConfig = async () => SimpleGit().raw(['describe'])
-  .then( _ =>
-    ({APP_NAME: PACKAGE.name, APP_VERSION: _})
-  ).catch( e => {
-    console.log(e);
-    return false }
-  );
-
-
-/*{
-try {
-  const lastGitTag = await SimpleGit().raw(['describe']);
-  return {APP_NAME: PACKAGE.name, APP_VERSION: lastGitTag}
-} catch (error) {
-  throw `Error: ${error}`
-  return false
-}
-}*/
-
+export const initConfig = () => ({ APP_NAME: PACKAGE.name, APP_VERSION: PACKAGE.gitCommitHash })
