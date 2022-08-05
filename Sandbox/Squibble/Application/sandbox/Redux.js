57ffe6d23b7cc54fb1ce75ebfb1554e6e9668949
Wes Cravens <wcravens@cortex-it.com>
2022-08-05 13:59:34 -0500

    Remove kruft.

--- Sandbox/Squibble/Application/sandbox/Redux.js
+++
@@ -1,5 +0,0 @@
-import redux from '@reduxjs/toolkit'
-
-const Properties =  Object.getOwnPropertyNames( redux );
-const Methods = Properties.filter( _ => typeof redux[_] === 'function' )
-console.log( Methods )
