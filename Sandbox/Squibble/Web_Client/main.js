bcc0cb28c975f8d10a692b17c9960cff372f4fe0
Wes Cravens <wcravens@cortex-it.com>
2022-08-06 09:19:58 -0500

    Remove boilerplate content.

--- Sandbox/Squibble/Web_Client/main.js
+++ Sandbox/Squibble/Web_Client/main.js
@@ -9,20 +9,4 @@ import {
 } from "./StyleThemes.js";
 
 insertStyleElementWithTheme( 'Bootstrap' );
-//switchTheme( 'Sketchy'  )
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
+//switchTheme( 'Sketchy'  );
