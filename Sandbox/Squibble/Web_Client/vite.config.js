7305ba123df476bc7e24a86ff93f3be25957a493
Wes Cravens <wcravens@cortex-it.com>
2022-08-06 05:40:58 -0500

    Patch up Web_Client Vite environment.

--- Sandbox/Squibble/Web_Client/vite.config.js
+++ Sandbox/Squibble/Web_Client/vite.config.js
@@ -1,19 +1,24 @@
 import * as path from 'path'
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import config from "../Application/Config.js"
+import gitInfo from 'rollup-plugin-git-info';
+import eslint from '@rollup/plugin-eslint';
 
 export default defineConfig( async( {command, mode } ) => {
-  const response = await loadConfig()
-  const APP_NAME = response.APP_NAME
-  const APP_VERSION = response.APP_VERSION
   return {
-    plugins: [ createHtmlPlugin( { inject: { data: { APP_NAME, APP_VERSION  } } } ) ],
+    plugins: [ createHtmlPlugin( { inject: {
+      data: {
+        //config.Application.name
+      }
+    } } ) ],
     define: {
       APP_VERSION: JSON.stringify( response.APP_VERSION ),
       APP_NAME:    JSON.stringify( response.APP_NAME )
     },
     resolve: {
       alias: {
-        '~bootstrap': path.resolve( __dirname, 'node_modules/bootstrap' ),
+        '~bootstrap':  path.resolve( __dirname, 'node_modules/bootstrap' ),
         '~bootswatch': path.resolve( __dirname, 'node_modules/bootswatch' ),
       }
     }
