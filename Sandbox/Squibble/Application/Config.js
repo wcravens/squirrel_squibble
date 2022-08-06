6517f8ed335b34e6b3e8a39b5f1e5b8f4a649571
Wes Cravens <wcravens@cortex-it.com>
2022-08-06 09:18:59 -0500

    Freeze config object.

--- Sandbox/Squibble/Application/Config.js
+++ Sandbox/Squibble/Application/Config.js
@@ -3,11 +3,10 @@ import { execSync } from 'child_process';
 
 const PACKAGE = JSON.parse( readFileSync( './package.json', { encoding: "utf-8" } ) );
 
-export default ({
+export default Object.freeze( {
   Application: {
     name: PACKAGE.name,
     version: PACKAGE.version,
     build_id: execSync( 'git describe', { encoding: "utf-8" } ).trim()
   }
-});
-
+} );
