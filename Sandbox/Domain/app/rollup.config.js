79836d0e502e86c189ccd96baa4a3999ae048b47
Wes Cravens <wcravens@cortex-it.com>
2022-08-21 13:06:50 -0500

    I broke it, but then I fixed it.

--- Sandbox/Domain/app/rollup.config.js
+++ Sandbox/Domain/app/rollup.config.js
@@ -6,6 +6,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import eslint from '@rollup/plugin-eslint';
+import del from 'rollup-plugin-delete';
 import pgk from './package.json';
 
 const isProduction = false;
@@ -13,37 +14,29 @@ const isProduction = false;
 export default ( async () => ({
   input: "src/index.js",
   output: {
-    file: "_build/bundle.js",
+    file: "_build/bundle.mjs",
     format: "es",
     sourcemap: true,
   },
-  onwarn(warning, warn) {
-    if (warning.code === 'THIS_IS_UNDEFINED') return;
-    warn(warning);
-  },
   plugins: [
-    nodeResolve({
-      extensions: [".js"],
+    del( { targets: "_build/*" }),
+    eslint(),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: "**/node_modules/**"
     }),
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify( 'development' )
     }),
-    eslint(),
-    babel({
-      env: { development: { compact: false } },
-      exclude: "**/node_modules/**",
-      presets: ["@babel/preset-react"],
-      babelHelpers: 'runtime'
-    }),
+    nodeResolve({ extensions: [".js"] }),
     commonjs(),
     isProduction && (await import('rollup-plugin-terser')).terser(),
     serve({
-      open: true,
       verbose: true,
       host: "localhost",
       port: 3000,
     }),
-    livereload({ watch: "dist" }),
+    livereload({ watch: "_build" }),
   ]
 }))();
